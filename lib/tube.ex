8312b3a0f26ecc26063c411697dfb2369ef1572d
Rainer Du <dusiyh@gmail.com>
2016-06-15 23:22:34 +0800

    defined call/3 and invoke/3 to Tube

--- lib/tube.ex
+++ lib/tube.ex
@@ -1,9 +1,9 @@
 defmodule Tube do
   @type opts :: tuple | atom | integer | float | [opts]
 
-  defmacro __using__ do
+  defmacro __using__(opts) do
     quote do
-      use Tube.Builder
+      use Tube.Builder, unquote(opts)
       import Tube.Context
     end
   end
@@ -11,4 +11,16 @@ defmodule Tube do
   @callback init(opts) :: opts
   @callback call(Tube.Context.t, opts) :: Tube.Context.t
 
+  defmacro call(tube, context, opts \\ []) do
+    quote bind_quoted: [tube: tube, context: context, opts: opts] do
+      tube.call(Tube.Context.context(context), opts)
+    end
+  end
+
+  defmacro invoke(tube, context, opts \\ [], key) when is_atom(key) do
+    quote bind_quoted: [tube: tube, context: context, opts: opts, key: key] do
+      context = tube.call(Tube.Context.context(context), opts)
+      Tube.Context.fetch!(context, key)
+    end
+  end
 end
--- test/tube_test.exs
+++ test/tube_test.exs
@@ -1,5 +1,24 @@
 defmodule TubeTest do
   use ExUnit.Case
-  doctest Tube
 
+  require Tube
+  import Tube.Context
+
+  defmodule Sample do
+    import Tube.Context
+
+    def call(context, opts) do
+      stack = [{:sample, opts}|fetch!(context, :stack)]
+      assign(context, :stack, stack)
+    end
+
+  end
+
+  test "export call/3" do
+    assert Tube.call(Sample, [stack: []], :opts) |> fetch!(:stack) == [{:sample, :opts}]
+  end
+
+  test "export invoke/3" do
+    assert Tube.invoke(Sample, [stack: []], :opts, :stack) == [{:sample, :opts}]
+  end
 end
